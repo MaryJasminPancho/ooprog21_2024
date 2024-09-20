855db4c94beffbac6e6e2d900c6c9403435c28fb
Mary Jasmin <maryjasminpancho@gmail.com>
2024-09-20 21:10:04 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,12 @@
+public class FirstAddressExercise {
+    public static void main(String[] args) {
+        System.out.println("First Java application\n");
+        displayAddress();
+    } 
+
+    public static void displayAddress(){
+        System.out.println("Samsung Electronics Co., Ltd.");
+        System.out.println("11 Seocho-daero 74-gil\nSeocho District\nSeoul, South Korea");
+        System.out.println("Company: Samsung");
+    }
+}
