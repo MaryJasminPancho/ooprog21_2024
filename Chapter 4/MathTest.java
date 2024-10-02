79065587ed8bcd547b3885dc21dff52cb43c105a
Mary Jasmin <maryjasminpancho@gmail.com>
2024-10-02 19:18:59 +0700

    Add files via upload

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+
+public class MathTest {
+   public static void main(String[] a) {
+      System.out.println("SQRT: " + Math.sqrt(25));
+      System.out.println("SIN: " + Math.sin(33));
+      System.out.println("COS: " + Math.cos(40));
+      System.out.println("FLOOR: " + Math.floor(25));
+      System.out.println("CEIL: " + Math.ceil(30));
+      System.out.println("ROUND: " + Math.round(25));
+      System.out.println("MAX: " + Math.max(25, 30));
+      System.out.println("MIN: " + Math.min(25, 28));
+      System.out.println("RANDOM: " + Math.random());
+   }
+}
